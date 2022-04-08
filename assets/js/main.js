c961609a6510b56c1c1f5aa9ef0e65e3b186f4fb
Brandon-Schefstad <bschefstad@gmail.com>
2022-04-08 08:43:39 -0400

    Mobile updateD

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,13 @@ function isPossiblyValidEmail(txt) {
 function acceptEmailInput() {
   const email = document.querySelector('.card__email__input').value
   if (isPossiblyValidEmail(email)){
-    return
+    alert(`Thank you for signing up using ${email}!`)
   }else {
-    document.querySelector('.card__email__input').classList.add('error')
+    document.querySelector('.card__email__input--error').classList.add('visible');
+    document.querySelector('.card__email__input--error').classList.remove('hidden')
+    document.querySelector('.card__email__input--error__text').classList.remove('hidden')
+    document.querySelector('card__email__input--error__text').classList.add('visible')
+   
   }
   console.log(`${email}`)
 }
