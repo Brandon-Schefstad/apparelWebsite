934c721b8aaf8f5fa90d66951c404a6bd5c6cac4
Brandon-Schefstad <bschefstad@gmail.com>
2022-04-08 08:00:17 -0400

    Updating

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,14 @@
-
-
-
-
+document.querySelector(".card__email__button").addEventListener('click', acceptEmailInput);
 function isPossiblyValidEmail(txt) {
   return txt.length > 5 && txt.indexOf('@')>0;
+}
+
+function acceptEmailInput() {
+  const email = document.querySelector('.card__email__input').value
+  if (isPossiblyValidEmail(email)){
+    return
+  }else {
+    document.querySelector('.card__email__input').classList.add('error')
+  }
+  console.log(`${email}`)
 }
